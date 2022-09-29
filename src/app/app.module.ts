9e156b4c0e82454426bc6a086abb5a9ff795b813
mohamad-mahvash <mohammadmahvash1@gmail.com>
2022-09-29 09:11:25 +0330

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRouting } from './app.routing';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRouting,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
