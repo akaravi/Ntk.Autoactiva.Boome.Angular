b451a96d348175add8899dc82d0a97662f522a59
mohamad-mahvash <mohammadmahvash1@gmail.com>
2022-10-01 10:33:24 +0330

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,10 @@ import { FruhstuckComponent } from './pages/core/fruhstuck/fruhstuck.component';
 import { InformationComponent } from './pages/core/information/information.component';
 import { KontaktComponent } from './pages/core/kontakt/kontakt.component';
 import { SightseeingComponent } from './pages/core/sightseeing/sightseeing.component';
-import { RateStarComponent } from './pages/layout/components/rate-star/rate-star/rate-star.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RestaurantComponent } from './pages/core/restaurant/restaurant.component';
 import { CheckinOutComponent } from './pages/core/checkin-out/checkin-out.component';
+import { RateStarComponent } from './pages/layout/components/rate-star/rate-star.component';
 
 @NgModule({
   declarations: [
