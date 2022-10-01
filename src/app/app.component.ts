746a10297362044c26817a1280c6190fbfa70ed2
Alireza Karavi <karavi.alireza@gmail.com>
2022-10-01 10:05:14 +0330

    indo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ntk.Autoactiva.Boome.Angular';
+  title = 'Autoactiva Boome';
 }
